37f5a6b08ca131875f7438595e572a5313c29cdb
Rio Prayoga <yoprayogap@gmail.com>
2024-01-10 23:03:06 +0700

    fix: remove library

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -22,7 +21,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  devTools: isProduction ? false : composeWithDevTools(),
+  // devTools: isProduction ? false : composeWithDevTools(),
 });
 
 // store.__persistor = persistStore(store);
