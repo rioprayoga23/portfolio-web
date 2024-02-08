b4a625232b4416054155f6fffcce8e80ddf66aaf
Rio Prayoga <yoprayogap@gmail.com>
2024-02-08 15:40:57 +0700

    feat: magentic wraper

--- src/components/About/components/Skills.jsx
+++ src/components/About/components/Skills.jsx
@@ -5,6 +5,7 @@ import { skills_data } from "@/data/skills";
 import SectionLayout from "@/components/core/Layout/SectionLayout";
 import CTitle from "@/components/core/Title";
 import Skill from "../partials/Skill";
+import Magnetic from "@/components/core/Magnetic";
 
 const Skills = () => {
   return (
@@ -14,7 +15,14 @@ const Skills = () => {
       {/* </h5> */}
       <div className="grid grid-cols-3 md:grid-cols-4 gap-5 mt-5">
         {skills_data.map((item, index) => (
-          <Skill src={item.icon} alt={item.alt} name={item.name} key={index} />
+          <Magnetic key={index}>
+            <Skill
+              src={item.icon}
+              alt={item.alt}
+              name={item.name}
+              key={index}
+            />
+          </Magnetic>
         ))}
       </div>
     </SectionLayout>
