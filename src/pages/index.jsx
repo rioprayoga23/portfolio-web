ab388f1d4b4ac5b3f37bcd3a58857a058afb3bd0
Rio Prayoga <yoprayogap@gmail.com>
2024-03-04 21:04:42 +0700

    feat: add meta title tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import dynamic from "next/dynamic";
 
+import { portfolioData } from "@/data/portfolios";
 import useSetTheme from "@/hooks/useSetTheme";
 
 import { WorkspaceSpinner } from "@/components/Workspace3d/loader";
 import ContentLayout from "@/components/core/Layout/ContentLayout";
 import MainLayout from "@/components/core/Layout/MainLayout";
-import { portfolioData } from "@/data/portfolios";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 const Workspace3d = dynamic(
   () => import("@/components/Workspace3d/workspace"),
@@ -17,9 +19,20 @@ const Workspace3d = dynamic(
 
 export default function Home({ detailPortfolio }) {
   useSetTheme();
+  const { asPath } = useRouter();
+
+  const title = Object.keys(detailPortfolio).length
+    ? `${detailPortfolio?.name} | Porfolio`
+    : asPath == "/portfolio"
+    ? "Porfolio"
+    : "Rio Prayoga | About";
 
   return (
     <MainLayout>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} key={title} />
+      </Head>
       <Workspace3d />
       <ContentLayout detailPortfolio={detailPortfolio} />
     </MainLayout>
